cf05c2065c307e74690308be9d4129b762dd46be
amos zohar <azamoszohar@gmail.com>
2025-01-16 17:22:45 +0200

    finished DSU implementation

--- DisjointSet.java
+++ DisjointSet.java
@@ -8,10 +8,12 @@ public class DisjointSet {
     public class Node {
         String key;
         Node next;
+        int rank;
 
         public Node(String key) {
             this.key = key;
             next = this;
+            this.rank = 0;
         }
 
         public Node(int i, int j) {
@@ -23,7 +25,16 @@ public class DisjointSet {
 
     public void CreateSingletonSet(int i, int j) {// Creates a Singleton Set comprised of only i,j.
         String key = CreateKey(i, j);
-        nodeMap.put(key, new Node(key));
+        if (!nodeMap.containsKey(key))
+            nodeMap.put(key, new Node(key));
+    }
+
+    /* a recursive aux method to assist Search method below */
+    private Node find(Node node) {
+        if (node != node.next) {
+            node.next = find(node.next);
+        }
+        return node.next;
     }
 
     /*
@@ -35,29 +46,24 @@ public class DisjointSet {
         String key = CreateKey(i, j);
         if (!nodeMap.containsKey(key))
             return null;
-        Node p = nodeMap.get(key);
-        Node root;
-        while (p != p.next)
-            p = p.next;
-        root = p;
-
-        p = nodeMap.get(key);
-        Node q;
-        while (p != p.next) {
-            q = p.next;
-            p.next = root;
-            p = q;
-        }
-        return root;
+        return find(nodeMap.get(key));
     }
 
     public Node Union(int i1, int j1, int i2, int j2) {
         Node root1 = Search(i1, j1);
         Node root2 = Search(i2, j2);
-        if (root1 != null && root2 != null) {
+        if (root1 == null || root2 == null)
+            return null;
+        if (root1 == root2)
+            return root1;
+        if (root1.rank < root2.rank) {
             root1.next = root2;
             return root2;
         }
-        return null;
+        if (root2.rank == root1.rank)
+            root1.rank++;
+
+        root2.next = root1;
+        return root1;
     }
 }
--- Hex.java
+++ Hex.java
@@ -1,10 +1,15 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Hex {
-    static final char vacant = 'G';
+    static final char vacant = '#';
     static final char playerTwo = 'B';
     static final char playerOne = 'W';
     DisjointSet player1 = new DisjointSet();
+    /*
+     * Invariant: player1 ∩ player2 = ∅ .
+     * That's why we have the search function in class DisjointSet
+     */
     DisjointSet player2 = new DisjointSet();
     char[][] board;
     int N;
@@ -20,43 +25,61 @@ public class Hex {
                 board[i][j] = vacant;
             }
         }
-        board[N / 2][0] = playerOne;
-        board[0][N / 2] = playerTwo;
     }
 
-    /* Given location XY and moves, specifies valid moves */
-    public ArrayList<Coordinates> availableMoveXY(Coordinates p) {
-        ArrayList<Coordinates> validMoves = new ArrayList<>();
-        int i = p.i;
-        int j = p.j;
-        if (i > 0)
-            validMoves.add(new Coordinates(i - 1, j));
-        if (i < N - 1)
-            validMoves.add(new Coordinates(i + 1, j));
-        if (j > 0)
-            validMoves.add(new Coordinates(i, j - 1));
-        if (j < N - 1)
-            validMoves.add(new Coordinates(i, j + 1));
+    public void printBoard() {
+        for (int i = 0; i < N; i++) {
+            System.out.print(" ".repeat(i));
+            for (int j = 0; j < N; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
-        /* in even indexes, can move up,down,left,right, and left+UP, right+UP */
-        if (j % 2 == 0) {
-            if (i > 0) {
-                if (j > 1)
-                    validMoves.add(new Coordinates(i - 1, j - 1));
-                if (j < N - 1)
-                    validMoves.add(new Coordinates(i - 1, j + 1));
+    public static void main(String[] args) {
+        Hex hex = new Hex(11);
+        hex.initGame();
+        hex.printBoard();
+        Scanner scanner = new Scanner(System.in);
+        Boolean gameRunning = true;
+        int turn = 0;// turn%2 == 0 ? player1 : player2.
+        while (gameRunning) {
+            int row = -1;
+            int col = -1;
+            boolean validMove = false;
+            while (!validMove) {
+                System.out.print("Turn " + turn + " , Player " + (turn % 2 == 0 ? playerOne
+                        : playerTwo) + " Enter row and column (0 to " + (hex.N - 1) + "): ");
+                if (scanner.hasNextInt()) {
+                    row = scanner.nextInt();
+                    if (scanner.hasNextInt()) {
+                        col = scanner.nextInt();
+                        if (row >= 0 && row < hex.N && col >= 0 && col < hex.N) {
+                            if (hex.board[row][col] == vacant) {
+                                validMove = true; // Move is valid and spot is empty
+                            } else {
+                                System.out.println("Spot already taken. Try again.");
+                            }
+                        } else {
+                            System.out.println("Coordinates out of bounds. Try again.");
+                        }
+                    } else {
+                        System.out.println("Invalid input. Please enter integers.");
+                        scanner.next(); // Clear invalid input
+                    }
+                } else {
+                    System.out.println("Invalid input. Please enter integers.");
+                    scanner.next(); // Clear invalid input
+                }
+            }
+            hex.board[row][col] = turn % 2 == 0 ? playerOne : playerTwo;
+            hex.printBoard();
+            turn++;
+            if (turn > 121) {
+                gameRunning = false;
+                scanner.close();
             }
         }
-        /* in even indexes, can move up,down,left,right, and left+DOWN, right+DOWN */
-        else {
-
-            if (i < N - 1) {
-                if (j > 1)
-                    validMoves.add(new Coordinates(i + 1, j - 1));
-                if (j < N - 1)
-                    validMoves.add(new Coordinates(i + 1, j + 1));
-            }
-        }
-        return validMoves;
     }
 }
